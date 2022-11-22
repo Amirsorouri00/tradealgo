079c33fac760e6440e1aa35483a5e1c14963f30a
Amir Hossein <a.soroori@metatude.org>
2022-11-23 00:34:17 +0330

    excersice 2 in the middle

---
+++ excercise 2/hurst.py
@@ -0,0 +1,61 @@
+# import matplotib.pyplot as plt
+import numpy as np
+# import pandas as pd
+import yfinance as yf
+
+
+def hurst(price, min_lag=2, max_lag=100):
+    lags = np.arange(min_lag, max_lag + 1)
+    tau = [np.std(np.subtract(price[lag:], price[:-lag]))
+           for lag in lags]
+    m = np.polyfit(np.log10(lags), np.log10(tau), 1)
+    return m, lags, tau
+
+
+N = 10000
+rand = np.cumsum(np.random.randn(N) + 0.01)
+mr = np.cumsum(np.sin(np.linspace(0, N/3*np.pi, N))/2 + 1)
+tr = np.cumsum(np.arange(N)/N)
+
+m_rand, lag_rand, rs_rand = hurst(rand)
+m_mr, lag_mr, rs_mr = hurst(mr)
+m_tr, lag_tr, rs_tr = hurst(tr)
+
+print(f"Hurst(Random):\t{m_rand[0]:.3f}")
+print(f"Hurst(MR):\t{m_mr[0]:.3f}")
+print(f"Hurst(TR):\t{m_tr[0]:.3f}")
+
+
+tickers = ['CHF=X', 'BTC-USD', 'SPY', 'GLD', 'USO']
+start = '2010-01-01'
+end = '2021-12-31'
+colors = ['red', 'green']
+yfObj = yf.Tickers(tickers)
+df = yfObj.history(start=start, end=end)
+df.drop(['Stock Splits', 'Dividends', 'Volume',
+         'Open', 'High', 'Low'], axis=1, inplace=True)
+df.columns = df.columns.swaplevel()
+
+vals = {c[0]: hurst(df[c].dropna().values) for c in df.columns}
+
+print(vals)
+# def plotHurst(m, x, y, series, name):
+#     fig, ax = plt.subplots(1, 2, figsize=(15, 6))
+#     ax[0].plot(np.log10(x), m[0] * np.log10(x) + m[1])
+#     ax[0].scatter(np.log10(x), np.log10(y), c=colors[1])
+#     ax[0].set_title(f"{name} (H = {m[0]:.3f})")
+#     ax[0].set_xlabel(r"log($\tau$)")
+#     ax[0].set_ylabel(r"log($\sigma_\tau$)")
+
+#     ax[1].plot(series)
+#     ax[1].set_title(f"{name}")
+#     ax[1].set_ylabel("Price ($)")
+#     ax[1].set_xlabel("Date")
+
+#     return fig, ax
+
+
+for k, v in vals.items():
+    print(v)
+    # fig, ax = plotHurst(*v, df[k], k)
+    # plt.show()
---
+++ excercise 2/sp_cadf.py
@@ -0,0 +1,83 @@
+import datetime
+
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from statsmodels.regression.linear_model import OLS
+from statsmodels.tsa.adfvalues import mackinnonp
+from statsmodels.tsa.tsatools import add_trend, lagmat
+
+SUB_SP_INDICES = ['IR', 'INTC', 'ICE', 'IP', 'IPG',
+                  'IFF', 'INTU', 'ISRG', 'IVZ', 'INVH', 'IQV']
+
+
+def adf(ts, maxlag=1):
+    """
+    Augmented Dickey-Fuller unit root test
+    """
+    # make sure we are working with an array, convert if necessary
+    ts = np.asarray(ts)
+
+    # Get the dimension of the array
+    nobs = ts.shape[0]
+
+    # Calculate the discrete difference
+    tsdiff = np.diff(ts)
+
+    # Create a 2d array of lags, trim invalid observations on both sides
+    tsdall = lagmat(tsdiff[:, None], maxlag, trim='both', original='in')
+    # Get dimension of the array
+    nobs = tsdall.shape[0]
+
+    # replace 0 xdiff with level of x
+    tsdall[:, 0] = ts[-nobs - 1:-1]
+    tsdshort = tsdiff[-nobs:]
+
+    # Calculate the linear regression using an ordinary least squares model
+    results = OLS(tsdshort, add_trend(tsdall[:, :maxlag + 1], 'c')).fit()
+    adfstat = results.tvalues[0]
+
+    # Get approx p-value from a precomputed table (from stattools)
+    pvalue = mackinnonp(adfstat, 'c', N=1)
+    return pvalue
+
+
+def cadf(x, y):
+    """
+    Returns the result of the Cointegrated Augmented Dickey-Fuller Test
+    """
+    # Calculate the linear regression between the two time series
+    ols_result = OLS(x, y).fit()
+
+    # Augmented Dickey-Fuller unit root test
+    return adf(ols_result.resid)
+
+
+def parser(s):
+    return datetime.strptime(s, '%Y-%m-%d')
+
+
+SP_INDICES = ['MMM', 'AOS', 'ABT', 'ABBV', 'ABMD', 'ACN', 'ATVI', 'ADM', 'ADBE', 'ADP', 'AAP', 'AES', 'AFL', 'A', 'APD', 'AKAM', 'ALK', 'ALB', 'ARE', 'ALGN', 'ALLE', 'LNT', 'ALL', 'GOOGL', 'GOOG', 'MO', 'AMZN', 'AMCR', 'AMD', 'AEE', 'AAL', 'AEP', 'AXP', 'AIG', 'AMT', 'AWK', 'AMP', 'ABC', 'AME', 'AMGN', 'APH', 'ADI', 'ANSS', 'AON', 'APA', 'AAPL', 'AMAT', 'APTV', 'ACGL', 'ANET', 'AJG', 'AIZ', 'T', 'ATO', 'ADSK', 'AZO', 'AVB', 'AVY', 'BKR', 'BALL', 'BAC', 'BBWI', 'BAX', 'BDX', 'WRB', 'BBY', 'BIO', 'TECH', 'BIIB', 'BLK', 'BK', 'BA', 'BKNG', 'BWA', 'BXP', 'BSX', 'BMY', 'AVGO', 'BR', 'BRO', 'BF.B', 'CHRW', 'CDNS', 'CZR', 'CPT', 'CPB', 'COF', 'CAH', 'KMX', 'CCL', 'CARR', 'CTLT', 'CAT', 'CBOE', 'CBRE', 'CDW', 'CE', 'CNC', 'CNP', 'CDAY', 'CF', 'CRL', 'SCHW', 'CHTR', 'CVX', 'CMG', 'CB', 'CHD', 'CI', 'CINF', 'CTAS', 'CSCO', 'C', 'CFG', 'CLX', 'CME', 'CMS', 'KO', 'CTSH', 'CL', 'CMCSA', 'CMA', 'CAG', 'COP', 'ED', 'STZ', 'CEG', 'COO', 'CPRT', 'GLW', 'CTVA', 'CSGP', 'COST', 'CTRA', 'CCI', 'CSX', 'CMI', 'CVS', 'DHI', 'DHR', 'DRI', 'DVA', 'DE', 'DAL', 'XRAY', 'DVN', 'DXCM', 'FANG', 'DLR', 'DFS', 'DISH', 'DIS', 'DG', 'DLTR', 'D', 'DPZ', 'DOV', 'DOW', 'DTE', 'DUK', 'DD', 'DXC', 'EMN', 'ETN', 'EBAY', 'ECL', 'EIX', 'EW', 'EA', 'ELV', 'LLY', 'EMR', 'ENPH', 'ETR', 'EOG', 'EPAM', 'EQT', 'EFX', 'EQIX', 'EQR', 'ESS', 'EL', 'ETSY', 'RE', 'EVRG', 'ES', 'EXC', 'EXPE', 'EXPD', 'EXR', 'XOM', 'FFIV', 'FDS', 'FAST', 'FRT', 'FDX', 'FITB', 'FRC', 'FE', 'FIS', 'FISV', 'FLT', 'FMC', 'F', 'FTNT', 'FTV', 'FBHS', 'FOXA', 'FOX', 'BEN', 'FCX', 'GRMN', 'IT', 'GEN', 'GNRC', 'GD', 'GE', 'GIS', 'GM', 'GPC', 'GILD', 'GL', 'GPN', 'GS', 'HAL', 'HIG', 'HAS', 'HCA', 'PEAK', 'HSIC', 'HSY', 'HES', 'HPE', 'HLT', 'HOLX', 'HD', 'HON', 'HRL', 'HST', 'HWM', 'HPQ', 'HUM', 'HBAN', 'HII', 'IBM', 'IEX', 'IDXX', 'ITW', 'ILMN', 'INCY',
+              'IR', 'INTC', 'ICE', 'IP', 'IPG', 'IFF', 'INTU', 'ISRG', 'IVZ', 'INVH', 'IQV', 'IRM', 'JBHT', 'JKHY', 'J', 'JNJ', 'JCI', 'JPM', 'JNPR', 'K', 'KDP', 'KEY', 'KEYS', 'KMB', 'KIM', 'KMI', 'KLAC', 'KHC', 'KR', 'LHX', 'LH', 'LRCX', 'LW', 'LVS', 'LDOS', 'LEN', 'LNC', 'LIN', 'LYV', 'LKQ', 'LMT', 'L', 'LOW', 'LUMN', 'LYB', 'MTB', 'MRO', 'MPC', 'MKTX', 'MAR', 'MMC', 'MLM', 'MAS', 'MA', 'MTCH', 'MKC', 'MCD', 'MCK', 'MDT', 'MRK', 'META', 'MET', 'MTD', 'MGM', 'MCHP', 'MU', 'MSFT', 'MAA', 'MRNA', 'MHK', 'MOH', 'TAP', 'MDLZ', 'MPWR', 'MNST', 'MCO', 'MS', 'MOS', 'MSI', 'MSCI', 'NDAQ', 'NTAP', 'NFLX', 'NWL', 'NEM', 'NWSA', 'NWS', 'NEE', 'NKE', 'NI', 'NDSN', 'NSC', 'NTRS', 'NOC', 'NCLH', 'NRG', 'NUE', 'NVDA', 'NVR', 'NXPI', 'ORLY', 'OXY', 'ODFL', 'OMC', 'ON', 'OKE', 'ORCL', 'OGN', 'OTIS', 'PCAR', 'PKG', 'PARA', 'PH', 'PAYX', 'PAYC', 'PYPL', 'PNR', 'PEP', 'PKI', 'PFE', 'PCG', 'PM', 'PSX', 'PNW', 'PXD', 'PNC', 'POOL', 'PPG', 'PPL', 'PFG', 'PG', 'PGR', 'PLD', 'PRU', 'PEG', 'PTC', 'PSA', 'PHM', 'QRVO', 'PWR', 'QCOM', 'DGX', 'RL', 'RJF', 'RTX', 'O', 'REG', 'REGN', 'RF', 'RSG', 'RMD', 'RHI', 'ROK', 'ROL', 'ROP', 'ROST', 'RCL', 'SPGI', 'CRM', 'SBAC', 'SLB', 'STX', 'SEE', 'SRE', 'NOW', 'SHW', 'SBNY', 'SPG', 'SWKS', 'SJM', 'SNA', 'SEDG', 'SO', 'LUV', 'SWK', 'SBUX', 'STT', 'STE', 'SYK', 'SIVB', 'SYF', 'SNPS', 'SYY', 'TMUS', 'TROW', 'TTWO', 'TPR', 'TRGP', 'TGT', 'TEL', 'TDY', 'TFX', 'TER', 'TSLA', 'TXN', 'TXT', 'TMO', 'TJX', 'TSCO', 'TT', 'TDG', 'TRV', 'TRMB', 'TFC', 'TYL', 'TSN', 'USB', 'UDR', 'ULTA', 'UNP', 'UAL', 'UPS', 'URI', 'UNH', 'UHS', 'VLO', 'VTR', 'VRSN', 'VRSK', 'VZ', 'VRTX', 'VFC', 'VTRS', 'VICI', 'V', 'VNO', 'VMC', 'WAB', 'WBA', 'WMT', 'WBD', 'WM', 'WAT', 'WEC', 'WFC', 'WELL', 'WST', 'WDC', 'WRK', 'WY', 'WHR', 'WMB', 'WTW', 'GWW', 'WYNN', 'XEL', 'XYL', 'YUM', 'ZBRA', 'ZBH', 'ZION', 'ZTS']
+
+full_stock_data = yf.download(SP_INDICES, '2020-11-01', '2021-11-01')
+
+# print(full_stock_data['Volume']['A'])
+stationary_series = []
+cnt = 0
+for i in SP_INDICES:
+    if cnt == (len(SP_INDICES)/2)+1:
+        break
+    cnt += 1
+    for j in SP_INDICES:
+        if i == j:
+            continue
+        try:
+            if cadf(full_stock_data['Volume'][i], full_stock_data['Volume'][j]) <= 0.05:
+                stationary_series.append([i, j])
+        except:
+            # print(i, j, "one of these doesn't exist")
+            continue
+
+
+print(stationary_series)
